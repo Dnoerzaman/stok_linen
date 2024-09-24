997e2bfc863f7134e525389f907163c2f81d826f
Dnoerzaman <noerzamand@gmail.com>
2024-09-24 11:00:22 +0700

    footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- footer.php -->
+<footer class="bg-blue-500 text-white py-4 mt-8">
+    <div class="container mx-auto text-center">
+        <p>&copy; 2024 Laundry Rumah Sakit Mata Bandung Eye Center. All rights reserved.</p>
+    </div>
+</footer>
